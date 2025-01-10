6559b6c889cb8564e53759b25f3e3b09d7b1f00f
Nicolas Oliveira Mor <nicolasoliveiramor05@gmail.com>
2025-01-09 23:35:03 -0300

    Teste unificado em um arquivo

--- cypress/e2e/agenda_de_contatos/agenda_contato_test.cy.js
+++ cypress/e2e/agenda_de_contatos/agenda_contato_test.cy.js
@@ -1,8 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Editando contato na agenda de contatos', () => {
+describe('Teste para a agenda de contatos', () => {
     beforeEach(() => {
         cy.visit('https://agenda-contatos-react.vercel.app')
+    })
+
+    it('Deve preencher o formulario', () => {
+        cy.get('input[placeholder="Nome"]').type('Nicolas')
+        cy.get('input[placeholder="E-mail"]').type('nicolas@gmail.com')
+        cy.get('input[placeholder="Telefone"]').type('11912345678')
+        cy.get('button[type="submit"]').click()
+    })
+
+    it('Verifica se o contato foi adicionado na lista', () => {
+        cy.get('.sc-iAEyYk > .sc-beqWaB').contains('Nicolas').should('exist', 'Nicolas')
     })
 
     it('Deve limpar os campos e alterar os dados', () => {
@@ -17,5 +28,10 @@ describe('Editando contato na agenda de contatos', () => {
         cy.get('.alterar').click()
     
         cy.get('.sc-eDDNvR > li').last().should('contain.text', 'nicolasmor@gmail.com')
+    })
+
+    it('Deve remover o primeiro contato da lista', () => {
+        cy.contains('Nicolas Mor').should('not.exist')
+        cy.get('.delete').last().click()        
     })
 })
